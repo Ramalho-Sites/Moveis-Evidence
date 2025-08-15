7976155002ea77e8df154d9145514656c83c5bb7
Davi Ramalho <142276947+Ramalho-Sites@users.noreply.github.com>
2025-08-14 22:23:47 -0300

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,22 @@
+# Moveis Evidence 
+
+Este é meu projeto com fotos e link para acessar online.
+
+🔗 **Acesse o site:** [Clique aqui](https://ramalho-sites.github.io/Moveis-Evidence/)
+
+---
+
+## 📸 Imagens do Projeto
+![Print da tela inicial](imagens/print-tela.png)
+
+
+
+
+![Página da cozinha](imagens/print-cozinha.png)
+
+---
+
+## Sobre
+
+### Um site feito para a empresa Moveis Evicence com o intuito de exibir os projetos ja feito da empresa para os clientes e um acesso facil aos contatos com a empresa.
+
